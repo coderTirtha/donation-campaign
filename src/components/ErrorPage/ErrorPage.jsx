930039ed54ab6543d88935bc225082c0127d0e9f
tirthadas9775 <75427198+tirthadas9775@users.noreply.github.com>
2023-09-26 17:10:31 +0600

    Error page furnished

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>404! Oops! Page not found!</h1>
+        <div className="flex flex-col justify-center items-center h-[100vh] space-y-4">
+            <h1 className="text-7xl font-bold">404</h1>
+            <h2 className="text-3xl font-semibold text-gray-500">Page not Found!</h2>
+            <p className="text-gray-400">The page you are trying to access, may has been deleted or moved to another directory!</p>
+            <Link to={'/'}><button className="btn">Go back to Home</button></Link>
         </div>
     );
 };
